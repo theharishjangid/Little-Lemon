ce7b0a0de0a313b502423aa19f43e80ffdb5c72a
Harish Jangid <harish.jangid@draup.com>
2023-12-30 14:29:42 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Header, Main, Footer } from './containers';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 function App() {
   return (
     <React.Fragment>
       <Header/>
-      <Main/>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Main />}></Route>
+          <Route path="/booking"></Route>
+        </Routes>
+      </BrowserRouter>
       <Footer/>
     </React.Fragment>
   );
